9036f326bf829ab67d63cec204662b40215fe86e
Morgan Sayers <morgansayers20298@gmail.com>
2023-05-23 18:54:39 -0500

    created get, post, put, and delete views in CommentController

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,12 +1,97 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpacePlace.Models;
+using SpacePlace.Repositories;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace SpacePlace.Controllers
 {
     public class CommentController : Controller
     {
+        private readonly ICommentRepository _repository;
+        public CommentController(ICommentRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var request = new CommentRequest()
+            {
+                ID = Guid.Empty
+            };
+            var response = _repository.GetComments(request);
+
+            if (!response.Success)
+            {
+                return RedirectToPage("Error");
+            }
+            return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult ViewComment([FromRoute] Guid id)
+        {
+            var request = new CommentRequest()
+            {
+                ID = id
+            };
+            var response = _repository.GetComments(request);
+
+            if (!response.Success)
+            {
+                return RedirectToPage("Error");
+            }
+            return View(response);
+        }
+
+        [HttpPost]
+        public IActionResult PostComment([FromForm] string poster, [FromForm] string content)
+        {
+            var request = new CommentRequest()
+            {
+                Poster = poster,
+                Content = content
+            };
+            var response = _repository.PostComment(request);
+
+            if (!response.Success)
+            {
+                return RedirectToPage("Error");
+            }
+            return View(response);
+        }
+
+        [HttpPut]
+        public IActionResult EditComment(Guid id, [FromForm] string content)
+        {
+            var request = new CommentRequest()
+            {
+                ID = id,
+                Content = content
+            };
+            var response = _repository.EditComment(request);
+
+            if (!response.Success)
+            {
+                return RedirectToPage("Error");
+            }
+            return View(response);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(Guid id)
+        {
+            var request = new CommentRequest()
+            {
+                ID = id
+            };
+            var response = _repository.DeleteComment(request);
+
+            if (!response.Success)
+            {
+                return RedirectToPage("Error");
+            }
+            return View(response);
         }
     }
 }
